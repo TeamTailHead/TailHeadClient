7849dbdf69412e458977f270adbca904e3242c94
Tekiter <tekiter8@gmail.com>
2022-10-25 23:44:42 +0900

    feat: 디자인 변경

--- src/components/join/JoinScreen.tsx
+++ src/components/join/JoinScreen.tsx
@@ -19,26 +19,72 @@ const JoinScreen: FC = () => {
   };
 
   return (
-    <Screen>
-      JoinScreen
-      <div>
-        <NicknameInput
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-        />
-        <button onClick={handleJoin}>JOIN</button>
-      </div>
-      <div>
+    <StyledScreen>
+      <CenterBox>
+        <Title>TailHead 끝말잇기</Title>
+        <NicknameForm>
+          <NicknameInput
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+            placeholder="닉네임 입력"
+          />
+          <JoinButton onClick={handleJoin}>시작하기</JoinButton>
+        </NicknameForm>
         {joinError.isError ? (
-          <p>접속에 실패했습니다: {joinError.message}</p>
+          <JoinErrorMessage>
+            접속에 실패했습니다: {joinError.message}
+          </JoinErrorMessage>
         ) : null}
-      </div>
-    </Screen>
+      </CenterBox>
+    </StyledScreen>
   );
 };
 
 export default JoinScreen;
 
+const StyledScreen = styled(Screen)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CenterBox = styled.div`
+  background-color: #ebebeb;
+  width: 400px;
+  padding: 20px 20px;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const NicknameForm = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
 const NicknameInput = styled.input`
   border: 1px solid grey;
+  flex-grow: 1;
+  padding: 0 10px;
+  border-radius: 5px;
+`;
+
+const JoinButton = styled.button`
+  background-color: #9bcdff;
+  margin-left: 5px;
+  padding: 5px 10px;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #89c4ff;
+  }
+`;
+
+const JoinErrorMessage = styled.p`
+  color: red;
+  display: block;
+  margin-top: 10px;
 `;
