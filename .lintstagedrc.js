df361d7bc312b767f54ab5ce84318102b27ba5f6
Tekiter <tekiter8@gmail.com>
2022-10-25 18:41:03 +0900

    chore: lintstaged 설정 변경

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   // "**/*.(ts|tsx)": "yarn tsc --noEmit",
-  "*.{js,jsx,ts,tsx}": "yarn lint:eslint",
-  "*.{js,jsx,ts,tsx}": "yarn lint:prettier",
+  "*.{js,jsx,ts,tsx}": [
+    "eslint --ignore-path .gitignore --resolve-plugins-relative-to . --fix",
+    "prettier --ignore-path .gitignore --write",
+  ],
 };
