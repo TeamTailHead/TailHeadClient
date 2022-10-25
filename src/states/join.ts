11d4d01283f3e257b9ab454895c2db0cf336e6e2
Tekiter <tekiter8@gmail.com>
2022-10-25 20:30:38 +0900

    feat: JoinStatus Atom 제거

--- src/states/join.ts
+++ src/states/join.ts
@@ -1,15 +1,5 @@
 import { atom } from "recoil";
 
-type JoinStatus =
-  | { status: "idle" }
-  | { status: "loading" }
-  | { status: "error"; message: string };
-
-export const joinStatusAtom = atom<JoinStatus>({
-  key: "joinStatus",
-  default: { status: "idle" },
-});
-
 type JoinError =
   | {
       isError: false;
