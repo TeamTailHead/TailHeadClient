ef7c1a0d3b8f93d59d045be99589e0c894256ccd
rabbitmjh <rabbitmjh@naver.com>
2022-10-25 21:17:22 +0900

    feat: ResultWorker 추가

---
+++ src/components/worker/ResultWorker.tsx
@@ -0,0 +1,29 @@
+import { FC, useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+
+import { useSetMessageHandler } from "@/communicator/context/communicator";
+import { resultAtom } from "@/states/result";
+import { screenStateAtom } from "@/states/screen";
+
+const ResultWorker: FC = () => {
+  const setHandler = useSetMessageHandler();
+  const setScreenState = useSetRecoilState(screenStateAtom);
+  const setResult = useSetRecoilState(resultAtom);
+
+  useEffect(() => {
+    setHandler("gameResult", ({ players }) => {
+      setResult(
+        players.map((player) => ({
+          id: player.id,
+          nickname: player.nickname,
+          score: player.score,
+        }))
+      );
+    });
+    setScreenState("result");
+  }, []);
+
+  return null;
+};
+
+export default ResultWorker;
