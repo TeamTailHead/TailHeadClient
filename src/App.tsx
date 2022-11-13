2d28c7479d4c47233aa0f51f806ff9fd81e4aac7
Tekiter <tekiter8@gmail.com>
2022-11-14 02:31:14 +0900

    fix: 타입 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import ConnectionStatus from "./components/connection/ConnectionStatus";
 import JoinScreen from "./components/join/JoinScreen";
 import LobbyScreen from "./components/lobby/LobbyScreen";
 import ResultScreen from "./components/result/ResultScreen";
-import { connectionStateAtom } from "./states/connection";
 import { ScreenState, screenStateAtom } from "./states/screen";
 import GlobalStyle from "./styles/GlobalStyle";
 
@@ -17,17 +16,14 @@ const screens: Array<{ key: ScreenState; component: ReactElement }> = [
   { key: "result", component: <ResultScreen /> },
 ];
 
-type ConnectionStatus = "connecting" | "connected" | "disconnected" | "error";
-
 const App: React.FC = () => {
   const screenState = useRecoilValue(screenStateAtom);
-  const connectionStatus = useRecoilValue(connectionStateAtom);
 
   return (
     <StyledApp>
       <GlobalStyle />
       <ScreenSelector selectedKey={screenState} screens={screens} />
-      <ConnectionStatus status={connectionStatus} />
+      <ConnectionStatus />
     </StyledApp>
   );
 };
