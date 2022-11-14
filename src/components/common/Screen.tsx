8421bc75c60d4bc3d24375ade6e2aa19631b1459
Tekiter <tekiter8@gmail.com>
2022-11-15 02:54:38 +0900

    feat: 접속 창 디자인 변경

--- src/components/common/Screen.tsx
+++ src/components/common/Screen.tsx
@@ -3,6 +3,9 @@ import styled from "@emotion/styled";
 const Screen = styled.div`
   height: 100%;
   width: 100%;
+
+  /* background: linear-gradient(26deg, #1e5570 0%, #69afe7 100%); */
+  background: linear-gradient(26deg, #7404f1 0%, #e18fa5 100%);
 `;
 
 export default Screen;
---
+++ src/styles/glass.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+export const glassCardStyle = css`
+  backdrop-filter: blur(16px) saturate(180%);
+  -webkit-backdrop-filter: blur(16px) saturate(180%);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 12px;
+  border: 1px solid rgba(209, 213, 219, 0.3);
+`;
