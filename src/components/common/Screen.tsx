256e606bb589addf4bcb5b11dad627066ea0e2ca
Tekiter <tekiter8@gmail.com>
2022-11-15 10:15:24 +0900

    feat: 배경 이미지 변경

--- src/components/common/Screen.tsx
+++ src/components/common/Screen.tsx
@@ -4,8 +4,12 @@ const Screen = styled.div`
   height: 100%;
   width: 100%;
 
-  /* background: linear-gradient(26deg, #1e5570 0%, #69afe7 100%); */
-  background: linear-gradient(26deg, #7404f1 0%, #e18fa5 100%);
+  /* background: linear-gradient(26deg, #7404f1 0%, #e18fa5 100%); */
+  background: url(https://images.unsplash.com/photo-1468276311594-df7cb65d8df6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80);
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export default Screen;
